622234f04bc5f693e1397bd289bb99d98361752f
Aaron Jacobson <awjacobson@users.noreply.github.com>
2024-06-11 22:11:20 -0500

    Update launch.cmd

--- launch.cmd
+++ launch.cmd
@@ -7,6 +7,8 @@ start https://7daystodie-servers.com/server/124886/
 start https://rust-servers.net/server/166985/
 start https://rust-servers.net/server/172248/
 start https://rust-servers.net/server/172247/
+start https://rust-servers.net/server/172892/
 start https://conan-exiles.com/server/89991/
+start https://ark-servers.net/server/350672/
 start https://ark-servers.net/server/330774/
 start https://dayz-servers.org/server/121736
