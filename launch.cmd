ad67d2ae06b1245fcb8c51a2a6d9e8f862ef5d07
Aaron Jacobson <awjacobson@users.noreply.github.com>
2025-06-17 22:23:30 -0500

    Update launch.cmd

--- launch.cmd
+++ launch.cmd
@@ -1,13 +1,15 @@
 start https://7daystodie-servers.com/server/115464/
+start https://7daystodie-servers.com/server/155651/
 start https://7daystodie-servers.com/server/124883/
 start https://7daystodie-servers.com/server/124884/
-start https://7daystodie-servers.com/server/124758/
 start https://7daystodie-servers.com/server/124885/
 start https://7daystodie-servers.com/server/124886/
 start https://rust-servers.net/server/166985/
 start https://rust-servers.net/server/172248/
 start https://rust-servers.net/server/172892/
 start https://conan-exiles.com/server/89991/
+start https://conan-exiles.com/server/91223/
+start https://conan-exiles.com/server/91224/
 start https://ark-servers.net/server/350672/
 start https://ark-servers.net/server/330774/
 start https://ark-servers.net/server/375918/
